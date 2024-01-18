346da751acc122c71d58e6b4c9fd1a36b0b72505
k1mjunyoung <me@kimjunyoung.com>
2024-01-18 23:42:09 +0900

    Initial commit

---
+++ src/main/java/InflearnSpringCoreBasic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package InflearnSpringCoreBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InflearnSpringCoreBasic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package InflearnSpringCoreBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
