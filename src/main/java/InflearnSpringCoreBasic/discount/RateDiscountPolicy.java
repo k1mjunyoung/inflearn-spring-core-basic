8a80d01afeffe9de04eddae98ebd53bf9af23a49
k1mjunyoung <me@kimjunyoung.com>
2024-01-24 23:23:23 +0900

    feat: 새로운 할인 정책 개발

---
+++ src/main/java/InflearnSpringCoreBasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package InflearnSpringCoreBasic.discount;
+
+import InflearnSpringCoreBasic.member.Grade;
+import InflearnSpringCoreBasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
