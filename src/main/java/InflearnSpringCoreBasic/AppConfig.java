f372a7431d8cdb83f43c265602305929bef83036
k1mjunyoung <me@kimjunyoung.com>
2024-05-24 15:19:08 +0900

    feat: AppConfig 리팩터링

--- src/main/java/InflearnSpringCoreBasic/AppConfig.java
+++ src/main/java/InflearnSpringCoreBasic/AppConfig.java
@@ -1,7 +1,9 @@
 package InflearnSpringCoreBasic;
 
 
+import InflearnSpringCoreBasic.discount.DiscountPolicy;
 import InflearnSpringCoreBasic.discount.FixDiscountPolicy;
+import InflearnSpringCoreBasic.member.MemberRepository;
 import InflearnSpringCoreBasic.member.MemberService;
 import InflearnSpringCoreBasic.member.MemberServiceImpl;
 import InflearnSpringCoreBasic.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import InflearnSpringCoreBasic.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
